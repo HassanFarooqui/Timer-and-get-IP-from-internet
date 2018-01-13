9434f472294a462f36916fda73ea4556cad5c769
Hassan farooqui <hassan_farooqui92@yahoo.com>
2018-01-13 17:32:13 +0500

    make a small timer and get ip from internet

---
+++ Pods/Target Support Files/Pods-SocketApplication/Pods-SocketApplication-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SocketApplication : NSObject
+@end
+@implementation PodsDummy_Pods_SocketApplication
+@end
--- SocketApplication/ViewController.swift
+++ SocketApplication/ViewController.swift
@@ -7,19 +7,59 @@
 //
 
 import UIKit
+import SocketIOClientSwift
+
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var LblCounter: UILabel!
+    var timer = NSTimer()
+    
+    var second = 60
+    
+    func updatetimer()
+    {
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: ( #selector(ViewController.newtimer)), userInfo: nil, repeats: true)
+    
+    }
+    func newtimer() {
+        if second != 0 {
+        print("Something")
+        second -= 1
+        LblCounter.text = "\(second)"
+        }
+        else
+        {
+        timer.invalidate()
+            LblCounter.text = ""
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // getIPAddress()
+        updatetimer()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func getIPAddress()
+    {
+        let url = NSURL(string: "http://www.google.com")
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!) { (responseData, responseURl, error) -> Void in
+            guard let data = responseData where error == nil else {
+              
+                return
+            }
+            
+            let ip = NSString(data: data, encoding: NSUTF8StringEncoding) as! String
+          
+            print(data, ip)
     }
-
+        task.resume()
+        
 
 }
 
+}
